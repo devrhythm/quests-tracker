88842b2368cca2526a9d6ab85dc311c59698d431
SUMETH K <sumeth_dev@live.com>
2025-01-30 08:42:35 +0700

    added http serve and default routes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use quests_tracker::{config::config_loader, infrastructure::postgres::postgres_connection};
+use std::sync::Arc;
+
+use quests_tracker::{
+    config::config_loader,
+    infrastructure::{axum_http::http_serve::start, postgres::postgres_connection},
+};
 use tracing::{error, info};
 
 #[tokio::main(flavor = "current_thread")]
@@ -18,12 +23,16 @@ async fn main() {
     info!("ENV has been loaded");
 
     let postgres_pool = match postgres_connection::establish_connection(&dotenvy_env.database.url) {
-        Ok(pool ) => pool,
-        Err(e ) => {
+        Ok(pool) => pool,
+        Err(e) => {
             error!("Failed to establish connection to Postgres: {}", e);
             std::process::exit(1);
         }
     };
 
     info!("Postgres connection has been established");
+
+    start(Arc::new(dotenvy_env), Arc::new(postgres_pool))
+        .await
+        .expect("Failed to start server");
 }
